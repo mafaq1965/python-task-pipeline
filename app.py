5797b705cdb352be9ecb7ca58e34423a0a709117
Afaq Ahmad <121144044+mafaq1965@users.noreply.github.com>
2025-01-29 12:19:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Jenkins CI/CD Pipeline! version 2 testing again"
+    return "Hello, Welcome to jenkins ci /cd pipeline version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
